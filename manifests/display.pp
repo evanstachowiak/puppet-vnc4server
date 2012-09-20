061ef1a456f32317d541a7cb66b3592f57c950d2
Evan Stachowiak <evan.stachowiak@gmail.com>
2012-09-20 17:20:25 -0400

    add documentation

--- manifests/display.pp
+++ manifests/display.pp
@@ -1,3 +1,30 @@
+# Define vnc4server::display
+#
+# All install and configure vnc4server
+#
+# == Parameters
+#
+#  [*display_no*]
+#    Set display number that vnc4server will be running on
+#  [*user*]
+#    User the vncserver will be run under 
+#  [*display_name*]
+#    Name of the vnc display
+#  [*geometry*]
+#    Geometry of the display
+#  [*depth*]
+#    Color-depth the display of display
+#  [*options*]
+#    Pass in arbitrary options instead of setting config parameters
+#
+# == Examples
+#
+#  vnc4server::display { 'my-vnc-server':
+#    display_no => '5',
+#  }
+#
+# == Requires:
+#
 define vnc4server::display (
   $display_no,
   $user         = $vnc4server::params::user,
